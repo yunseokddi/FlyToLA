a8a051f9b123bd3a9853867753d22ed84bb990f0
yunseokddi <oys5339@naver.com>
2022-01-27 22:16:16 +0900

    NMF studying

--- chapter2/NMF.py
+++ chapter2/NMF.py
@@ -16,18 +16,18 @@ newsgroups_test = fetch_20newsgroups(subset='test', categories=categories, remov
 num_topics, num_top_words = 6, 8
 
 vectorizer = CountVectorizer(stop_words='english')
-vectors = vectorizer.fit_transform(newsgroups_train.data).todense()
+vectors = vectorizer.fit_transform(newsgroups_train.data)
 
 vocab = np.array(vectorizer.get_feature_names())
 
-m, n = vectors.shape
+# m, n = vectors.shape
 d = 5  # num topics
 
 clf = decomposition.NMF(n_components=d, random_state=1)
 
-W1 = clf.fit_transform(vectors)
-H1 = clf.components_
 
+# W1 = clf.fit_transform(vectors)
+# H1 = clf.components_
 
 def show_topics(a):
     top_words = lambda t: [vocab[i] for i in np.argsort(t)[:-num_top_words - 1: -1]]
@@ -35,4 +35,23 @@ def show_topics(a):
     return [' '.join(t) for t in topic_words]
 
 
+# print(show_topics(H1))
+# plt.plot(clf.components_[0])
+# plt.show()
+# print(clf.reconstruction_err_)
+
+# Goal: Minimized the Frobenius norm of V - WH
+
+vectorizer_tfids = TfidfVectorizer(stop_words='english')
+vectors_tfidf = vectorizer_tfids.fit_transform(newsgroups_train.data)
+
+# W1 = clf.fit_transform(vectors_tfidf)
+# H1 = clf.components_
+
+lam = 1e3
+lr = 1e-2
+m, n = vectors_tfidf.shape
+
+W1 = clf.fit_transform(vectors)
+H1 = clf.components_
 print(show_topics(H1))
